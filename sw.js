9d6b45f57d9588214f7678ebffbf19948558095b
David <dazcuenaga@gmail.com>
2026-09-02 15:30:07 +0200

    Fontanería: mover el badge 12 (gallinero) encima del 11 (izquierda porche)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'congelados-v47';
+const CACHE = 'congelados-v48';
 const SHELL = ['./', './index.html', './style.css', './app.js', './electricidad-data.js', './fontaneria-data.js', './config.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (e) => {
